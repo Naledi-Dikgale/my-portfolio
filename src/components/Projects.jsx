b8aa034a2e30513eb78f0d86e9b534270a3c9eee
naledi-dikgale <naledi22khosi@gmail.com>
2023-09-10 10:59:58 +0200

    Add modal pop-up

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,15 +1,46 @@
-import React from 'react'
+import { useState } from 'react'
 import { projects } from '../constants';
-import ProjectCard from './cards/Card';
+import ProjectCard from './cards/ProjectCard';
+import Modal from 'react-modal'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClose } from '@fortawesome/free-solid-svg-icons';
 const Projects = () => {
+  let [open, setOpen] = useState(false)
+  let [close, setClose] = useState(false)
+  let [card, setCard] = useState(null)
+  const handleClick = (crd) => {
+    setCard(crd)
+    setOpen(true)
+    setClose(false)
+  }
+  const handleClose = ()=>{
+    setClose(true);
+    setOpen(false)
+    return true
+  }
   return (
     <div id="projects" className="w-full p-6 h-fit flex flex-col justify-center items-center">
     <h2 className="text-4xl font-bold text-center underline underline-offset-4 my-5">Projects</h2>
       <div className="w-full h-max flex gap-5 h-max flex-wrap  items-center justify-center">
       {projects.map((project, index) => {
-        return <ProjectCard key={index} project={project} />;
-      })}
+          return <div onClick={() => handleClick( <ProjectCard project={project} />)} key={index} className="h-max w-max flex items-center justify-center border-slate-200"> <ProjectCard project={project} /></div>;
+        })}
     </div>
+    <Modal
+       isOpen = {open}
+       onRequestClose={close}
+         shouldCloseOnOverlayClick={true}
+       className="w-full flex flex-col items-center justify-center"
+       >
+      <div className="w-fit bg-primary rounded-xl p-4 relative">
+        <div className="w-full flex justify-end items-end absolute top-8 right-8">
+          <FontAwesomeIcon icon={faClose} onClick={handleClose} className="text-secondary h-6 w-6 mb-1" />
+        </div>
+        {
+          card
+        }
+      </div>
+    </Modal>
   </div>
   )
 }
---
+++ src/components/cards/ProjectCard.jsx
@@ -0,0 +1,48 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+
+const ProjectCard = (project) => {
+  const prj = { ...project };
+
+  return (
+    <div className="h-fit w-fit max-sm:w-full md:w-64 lg:w-full border  border-secondary text-secondary rounded-lg p-3 flex flex-col justify-center items-center hover:border-2 hover:border-white transition-transform  ">
+      <div className="flex flex-col items-center justify-center">
+        <img className="object-contain md:w-60 md:h-44 lg:w-64 lg:h-48 my-2" src={prj.project.imageURL} alt={prj.project.title} />
+        <h3 className="text-xl max-sm:text-lg font-bold underline underline-offset-4 text-center py-3">
+          {prj.project.name}
+        </h3>
+        <p className="lg:h-34 md:h-32 md:w-56 text-dimBlue text-justify">{prj.project.description}</p>
+        <div
+          className={`w-full flex flex-wrap py-1 gap-2 justify-center items-center font-bold h-max p-1`}
+        >
+          {prj.project.tech.map((element, index) => {
+            return (
+              <div
+                className={
+                  `py-1 px-2 box-border rounded border bg-secondary text-primary w-full hover:bg-primary hover:text-secondary hover:cursor-pointer ` +
+                  `md: w-max grid grid-cols-${prj.project.tech.length}`
+                }
+                key={index}
+              >
+                <span className="w-max h-max  text-center">
+                  {element.toUpperCase()}
+                </span>
+              </div>
+            );
+          })}
+        </div>
+        <div className="w-5/6 py-3 flex justify-between">
+          <a href={prj.project.projectSrc} target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faGithub}  className="h-6 w-6 hover:text-green-500 transition-transform hover:-translate-y-1"/>
+          </a>
+          <a href={prj.project.projectURL} target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faArrowUpRightFromSquare} className="h-6 w-6 hover:text-green-400 transition-transform hover:-translate-y-1" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
