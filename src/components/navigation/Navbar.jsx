ad9310a749c910ff88bb07f35768af84b1b04f3b
naledi-dikgale <naledi22khosi@gmail.com>
2023-10-31 19:59:55 +0200

    Change styling

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -12,9 +12,9 @@ const Navbar = () => {
   return (
     <nav
       id="nav"
-      className="bg-secondary text-neutral-50 transition-all w-full z-10 p-3 flex justify-end items-end fixed right-0 top-0"
+      className="bg-secondary text-neutral-50 transition-all w-full z-10 p-3 flex justify-end items-end fixed right-0 top-0 h-16"
     >
-      <div className="w-full flex justify-start items-center italic">Naledi</div>
+      <div className="w-full flex justify-start items-center font-lobster text-2xl ">Naledi</div>
       {
       !toggle ? (
         <FontAwesomeIcon
@@ -34,8 +34,8 @@ const Navbar = () => {
         />
       )}
 
-      <ul id="navlist" className="w-full z-50 flex gap-4 justify-end right-0 max-sm:hidden max-sm:flex-col max-sm:justify-start max-sm:py-24 max-sm:items-center max-sm:bg-primary max-sm:h-screen max-sm:w-full transition-all">
-        <li className="text-2xl cursor-pointer hover:-translate-y-1 hover:underline transition-all " onClick={toggleHandler}>
+      <ul id="navlist" className="w-full z-50 flex gap-4 justify-end right-0 max-sm:hidden max-sm:flex-col max-sm:justify-start max-sm:py-24 max-sm:items-center max-sm:bg-primary max-sm:h-screen max-sm:w-full transition-all font-lobster">
+        <li className="text-2xl cursor-pointer hover:-translate-y-1 hover:underline transition-all  " onClick={toggleHandler}>
           {" "}
           <a href="#about">About</a>
         </li>
