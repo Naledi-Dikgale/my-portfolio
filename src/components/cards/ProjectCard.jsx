1403eb8e11f251a85ec9d1b173c832baf9b6ec48
naledi-dikgale <naledi22khosi@gmail.com>
2023-11-02 21:47:48 +0200

    Finalize styling

--- src/components/cards/ProjectCard.jsx
+++ src/components/cards/ProjectCard.jsx
@@ -9,10 +9,10 @@ const ProjectCard = (project) => {
     <div className="h-fit w-fit max-sm:w-full md:w-64 lg:w-full border text-primary p-3 flex flex-col justify-center items-center border border-3 border-secondary rounded hover:border-2 hover:border-secondary transition-transform max-w-xs bg-primay ">
       <div className="flex flex-col items-center justify-center">
         <img className="object-contain md:w-60 md:h-44 lg:w-64 lg:h-48 my-2" src={prj.project.imageURL} alt={prj.project.title} />
-        <h3 className="text-2xl font-texturina text-pink-700 font-bold text-pink-700 underline underline-offset-4 text-center py-3">
+        <h3 className="text-xl font-agbalumo text-pink-700 font-bold text-pink-700 underline underline-offset-4 text-center py-3">
           {prj.project.name}
         </h3>
-        <p className="lg:h-34 md:h-32 md:w-56 text-dimBlue text-justify">{prj.project.description}</p>
+        <p className="lg:h-34 md:h-32 md:w-56 text-dimBlue text-justify font-texturina">{prj.project.description}</p>
         <div
           className={`w-full flex flex-wrap py-1 gap-2 justify-center items-center font-bold h-max p-1`}
         >
