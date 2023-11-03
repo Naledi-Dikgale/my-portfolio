226ca3e2edfb46e02c693505b72c7441112c2bc3
naledi-dikgale <naledi22khosi@gmail.com>
2023-11-03 19:19:22 +0200

    Finalize styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="text-center py-4 text-pink-700 font-lobster text-xl">
+    <footer className="text-center py-4 text-pink-400 font-lobster text-xl">
       <div>
         <p>&copy; Naledi Dikgale {currentYear}</p>
       </div>
