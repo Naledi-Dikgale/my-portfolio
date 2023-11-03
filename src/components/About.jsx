98195f0c48440ac643f8d946d908b29669887a50
naledi-dikgale <naledi22khosi@gmail.com>
2023-11-03 19:19:10 +0200

    Finalize styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
       <div className="flex flex-col max-sm:w-full text-justify md:w-2/3 items-start justify-center  p-10 rounded-lg shadow-[0_60px_50px_-40px_rgba(0,128,128,0.3)]">
         <div className="flex w-full gap-5 max-sm:flex-col">
           <div className="w-full">
-            <span className="text-pink-700 font-bold text-3xl font-agbalumo">About Me</span>
+            <span className="text-pink-400 text-3xl font-agbalumo font-bold">About Me</span>
             <p className="text-neutral-50 w-full py-3 font-medium font-texturina">
               Crafting digital dreams with code! As a full-stack developer, I&apos;m
               your one-stop destination for turning ideas into pixel-perfect
@@ -20,7 +20,7 @@ const About = () => {
           </div>
 
           <div className="max-sm:w-full md:w-full h-full">
-            <span className="text-pink-700 font-bold text-3x font-agbalumo">Skills</span>
+            <span className="text-pink-400 text-3x font-agbalumo font-bold">Skills</span>
             <div className="grid grid-rows-4 grid-cols-2 grid-flow-col w-full py-3 h-full">
               {skills.map((element, index) => {
                 return (
@@ -35,7 +35,7 @@ const About = () => {
         </div>
         <button
               type="button"
-              className="transition-all my-3  w-full h-16 text-neutral-50 bg-secondary hover:bg-pink-700 hover:text-neutral-50 "
+              className="transition-all my-3  w-full h-16 text-neutral-50 bg-secondary hover:bg-pink-400 hover:text-neutral-50 "
             >
               <a
                 href="#contact"
