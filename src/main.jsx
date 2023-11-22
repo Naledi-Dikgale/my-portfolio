a9638bebb64c7f96cf25d3f1502858b1c4e574f5
naledi-dikgale <naledi22khosi@gmail.com>
2023-11-22 22:40:30 +0200

    Add theme

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ThemeProvider } from './ThemeContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ThemeProvider>
+      <App />
     <App />
+    </ThemeProvider>
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
